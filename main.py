59682c4025b24bf97cc512c26b001bbc9c3fe20d
lucas <lucashonorato0093@gmail.com>
2023-03-15 21:12:42 -0300

    helo

---
+++ main.py
@@ -0,0 +1,10 @@
+while True:
+    nome = input('Qual seu nome senhor ?')
+    if (nome!= 'tuba'):
+        continue
+    senha = input('Digita a senha ai, rapidão')
+    if (senha == 'senhadificildemais'):
+        break
+print('Parabens acabamos de roubar seus dados ')
+
+'Aula Uninter, não recomento'
